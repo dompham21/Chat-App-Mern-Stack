37b5234dbc1ab192f0d21881a00753fd90c5d2c0
dompham21 <63381043+dompham21@users.noreply.github.com>
2020-11-27 10:48:22 +0700

    [1.0.0] deploy

--- client/src/components/ContactsModal/SearchUserTabPane/SearchUserTabPane.js
+++ client/src/components/ContactsModal/SearchUserTabPane/SearchUserTabPane.js
@@ -14,6 +14,12 @@ function SearchUserTabPane() {
     const [nameToSearch, setNameToSearch] = useState('')
     const [listUser, setListUser] = useState([]);
     const [loading,setLoading] = useState(false);
+    const [loadingAddContact,setLoadingAddContact] = useState(false);
+    const [loadingRemoveReq,setLoadingRemoveReq] = useState(false);
+    const [loadingRemoveContact,setLoadingRemoveContact] = useState(false);
+    const [loadingApproveFriend,setLoadingApproveFriend] = useState(false);
+    const [loadingDeleteFriend,setLoadingDeleteFriend] = useState(false);
+
     const refAddContact = useRef([]);
     const refRemoveContact = useRef([]);
     const refRemoveRequest = useRef([]);
@@ -37,21 +43,26 @@ function SearchUserTabPane() {
     }
     const handleAddContact = async (id) => {
         try {
+            setLoadingAddContact(true);
             let response = await dispatch(addContact(id))
             if(response.payload.addSuccess){
+                setLoadingAddContact(false)
                 refAddContact.current[id].style.display = "none";
                 refRemoveRequest.current[id].style.display = "block";
                 socket.emit('add-new-contact', {contactId:id})
             } 
         } catch (error) {
             console.log(error)
+            setLoadingAddContact(false)
         }             
     }
 
     const handleRemoveContact = async (id) => {
         try {
+            setLoadingRemoveContact(true);
             let response = await dispatch(removeContact(id))
             if(response.payload.removeSuccess){
+                setLoadingRemoveContact(false)
                 refAddContact.current[id].style.display = "block";
                 refRemoveContact.current[id].style.display = "none";
                 socket.emit('remove-contact',{contactId:id})
@@ -63,13 +74,16 @@ function SearchUserTabPane() {
 
     const handleRemoveRequest = async (id) => {
         try {
+            setLoadingRemoveReq(true);
             let response = await dispatch(removeContactReqReceived(id))
             if(response.payload.removeSuccess){
+                setLoadingRemoveReq(false);
                 refAddContact.current[id].style.display = "block";
                 refRemoveRequest.current[id].style.display = "none";
                 socket.emit('remove-req-contact-sent',{contactId:id})
             }
         } catch (error) {
+            setLoadingRemoveReq(false);
             console.log(error)
         }
         
@@ -77,28 +91,34 @@ function SearchUserTabPane() {
 
     const handleDeleteFriend = async (id) => {
         try {
+            setLoadingDeleteFriend(true);
             let response = await dispatch(removeContactReqReceived(id))
             if(response.payload.removeSuccess){
+                setLoadingDeleteFriend(false)
                 refAddContact.current[id].style.display = "block";
                 refApproveReq.current[id].style.display = "none";
                 refRemoveReceived.current[id].style.display = "none";
                 socket.emit('remove-req-contact-received',{contactId:id})
             }
         } catch (error) {
+            setLoadingDeleteFriend(false)
             console.log(error)
         }        
     }
 
     const handleApproveFriendReq = async (id) => {
         try {
+            setLoadingApproveFriend(true);
             let response = await dispatch(approveContactReqReceived(id))
             if(response.payload.approveSuccess){
+                setLoadingApproveFriend(false);
                 refRemoveContact.current[id].style.display = "block";
                 refApproveReq.current[id].style.display = "none";
                 refRemoveReceived.current[id].style.display = "none";
                 socket.emit('approve-request-contact-received',{contactId:id})
             }
         } catch (error) {
+            setLoadingApproveFriend(false);
             console.log(error)
         }
     }
@@ -132,6 +152,7 @@ function SearchUserTabPane() {
                                             ref={el => (refAddContact.current[item._id] = el)} 
                                             style={{display: !item.statusSend && !item.statusAdd ? "inline-block":"none"}}   
                                             onClick={()=>handleAddContact(item._id)}
+                                            loading={loadingAddContact}
                                         ><FaUserPlus/></div>
                                     </Badge>
                                 </Tooltip>
@@ -142,6 +163,7 @@ function SearchUserTabPane() {
                                             ref={el => (refRemoveRequest.current[item._id] = el)} 
                                             style={{display:item.statusAdd  && !item.status ?"inline-block":"none"}} 
                                             onClick={()=>handleRemoveRequest(item._id)}
+                                            loading={loadingRemoveReq}
                                         ><FaUserTimes/></div>
                                     </Badge>
                                 </Tooltip>
@@ -152,6 +174,7 @@ function SearchUserTabPane() {
                                         className="btn-delete-req"
                                         style={{display:(item.status && item.statusSend )|| (item.status && item.statusAdd) ? "inline-block":"none"}}
                                         onClick={()=>handleRemoveContact(item._id)}
+                                        loading={loadingRemoveContact}
                                     >Remove</Button>    
                                 </Tooltip>
                                 <div style={{display: !item.status &&  item.statusSend? "inline-block": "none"}}>
@@ -159,14 +182,16 @@ function SearchUserTabPane() {
                                         type="primary" 
                                         className="btn-confirm-req"
                                         ref={el => (refApproveReq.current[item._id] = el)} 
-
                                         onClick={()=>handleApproveFriendReq(item._id)}
+                                        loading={loadingApproveFriend}
+
                                     >Confirm</Button>
                                     <Button 
                                         type="primary" 
                                         className="btn-delete-req" 
                                         ref={el => (refRemoveReceived.current[item._id] = el)} 
                                         onClick={()=>handleDeleteFriend(item._id)}
+                                        loading={loadingDeleteFriend}
                                     >Delete</Button>  
                                 </div>
                         </List.Item>
